59e4e428f1653e0a9e1a301ed12c0552d26c3b79
Unknown <james.steer98@gmail.com>
2018-03-19 16:30:52 +0000

    Employed gitignore to ignore all unneeded changes
    usable branch for tracking changes to app

---
+++ CoreWarMobile/app/src/main/java/com/corewarmobile/corewarmobile/BarrackActivity.java
@@ -0,0 +1,55 @@
+package com.corewarmobile.corewarmobile;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BarrackActivity extends AppCompatActivity {
+
+    private static Context context;
+    private List<String> fileList = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        BarrackActivity.context = getApplicationContext();
+        setContentView(R.layout.activity_barrack);
+        File root = getFilesDir();
+        ListDir(root);
+        //String[] fileList(context.getFilesDir());
+        //String Root = context.getFilesDir();
+
+    }
+
+
+    void ListDir(File f) {
+        ListView lv;
+        File[] files = f.listFiles();
+        fileList.clear();
+        for (File file : files) {
+            fileList.add(file.getPath());
+        }
+        //ArrayAdapter<String> directoryList = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, fileList);
+        //setListAdapter(directoryList);
+
+        lv = (ListView)findViewById(R.id.ListView);
+        lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, fileList));
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                // Clicking on items
+            }
+        });
+    }
+
+}
---
+++ CoreWarMobile/app/src/main/java/com/corewarmobile/corewarmobile/GameActivity.java
@@ -0,0 +1,147 @@
+/*
+
+
+    GameActivity.java
+
+
+ */
+
+
+package com.corewarmobile.corewarmobile;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Matrix;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import java.lang.Runnable;
+import java.util.Random;
+import android.util.DisplayMetrics;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+
+public class GameActivity extends AppCompatActivity {
+    private static Context context;
+
+    SurfaceHolder surfaceHolder;
+    Canvas coreCanvas;
+    Rect canvasDimensions;
+    int count;
+    int id;
+    Paint paint = new Paint();
+    Rect rectangle;
+    Matrix identityMatrix;
+
+    int canvasWidth, canvasHeight;
+
+    Bitmap.Config conf = Bitmap.Config.ARGB_8888; // see other conf types
+    Bitmap bmp;
+    Canvas bufferCanvas;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GameActivity.context = getApplicationContext();
+        setContentView(R.layout.activity_game);
+
+        final SurfaceView surface = (SurfaceView) findViewById(R.id.CoreSurface);
+        final TextView progress = (TextView) findViewById(R.id.progressLabel);
+        final Handler handler = new Handler();
+        identityMatrix = new Matrix();
+
+        final Runnable loop = new Runnable() {
+            @Override
+            public void run() {
+
+
+                id = count;
+                progress.setText("Start " + id);
+
+                coreCanvas = surfaceHolder.lockCanvas();
+
+                Random rnd = new Random();
+                int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+
+                rectangle = new Rect(count, 100, count+100, 200);
+                count+=100;
+                paint.setColor(color);
+
+                bufferCanvas.drawRect(rectangle, paint);
+                coreCanvas.drawBitmap(bmp, identityMatrix, null);
+
+                progress.setText("End " + id);
+                surfaceHolder.unlockCanvasAndPost(coreCanvas);
+
+                if (count < 1000) {
+                    handler.postDelayed(this, 500);
+
+                }
+            }
+        };
+
+        surface.getHolder().addCallback(new SurfaceHolder.Callback() {
+            public void surfaceCreated(SurfaceHolder holder) {
+
+
+                surfaceHolder = holder;
+                canvasDimensions = holder.getSurfaceFrame();
+
+                canvasWidth = canvasDimensions.width();
+                canvasHeight = canvasDimensions.height();
+
+                bmp = Bitmap.createBitmap(canvasWidth, canvasHeight, conf); // this creates a MUTABLE bitmap
+                bufferCanvas = new Canvas(bmp);
+                // Do some drawing when surface is ready
+
+                progress.setText("Post");
+
+                coreCanvas = surfaceHolder.lockCanvas();
+
+                for (int i = 0; i < 1000; i+=100) {
+
+                    Random rnd = new Random();
+                    int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                    Rect rectangle = new Rect(i, 0, i+100, 100);
+                    paint.setColor(color);
+
+                    bufferCanvas.drawRect(rectangle, paint);
+
+                }
+                coreCanvas.drawBitmap(bmp, identityMatrix, null);
+                surfaceHolder.unlockCanvasAndPost(coreCanvas);
+
+                count = 0;
+                handler.postDelayed(loop, 500);
+            }
+
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                handler.removeCallbacks(loop);
+            }
+
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+        });
+    }
+
+
+    public void NewWarrior(View view){
+        Intent NewW = new Intent(this, NewWarriorActivity.class);
+        startActivity(NewW);
+    }
+    public void Add(View view){
+        Intent Barracks = new Intent(this, BarrackActivity.class);
+        startActivity(Barracks);
+    }
+
+}
