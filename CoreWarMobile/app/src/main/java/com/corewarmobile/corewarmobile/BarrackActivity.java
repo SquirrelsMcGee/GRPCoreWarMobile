54121b7797b0f23d46e4d9aa80361bab92342ec6
psygwc <psygwc@exmail.nottingham.ac.uk>
2018-05-03 17:55:52 +0100

    Added onclick functionality, to store warrior name and open game activity

--- CoreWarMobile/app/src/main/java/com/corewarmobile/corewarmobile/BarrackActivity.java
+++ CoreWarMobile/app/src/main/java/com/corewarmobile/corewarmobile/BarrackActivity.java
@@ -1,6 +1,7 @@
 package com.corewarmobile.corewarmobile;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,13 +10,17 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BarrackActivity extends AppCompatActivity {
 
+    public String item;
     private static Context context;
     private List<String> fileList = new ArrayList<String>();
 
@@ -45,11 +50,21 @@ public class BarrackActivity extends AppCompatActivity {
         lv = (ListView)findViewById(R.id.ListView);
         lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, fileList));
 
+        final Intent PlayS = new Intent(this, GameActivity.class);
+
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
                 // Clicking on items
+                item = ((TextView)v).getText().toString();
+                File newW = new File(item);
+                GameActivity.WarriorName = item;
+                Toast.makeText(getBaseContext(), item, Toast.LENGTH_SHORT).show();
+                startActivity(PlayS);
+                //GA.TextChanger(item);
+
             }
         });
+
     }
 
 }
